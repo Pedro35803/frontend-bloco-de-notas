3772d283bc9a8c86c86d62ebbe43f3efb12ccabf
Pedro <pedrojuniorifpb@gmail.com>
2023-12-29 18:04:15 -0300

    Lógica temporaria

--- src/services/refreshToken.js
+++ src/services/refreshToken.js
@@ -1,11 +1,11 @@
 import { createAxiosInstance } from "../api.js";
 import { getRefreshToken } from "./cookiesHandle.js";
 
-export const refreshToken = async ({ refresh }) => {
+export const refreshToken = async () => {
     const token = getRefreshToken()
     const api = createAxiosInstance(token);
 
-    const response = await api.post("/refresh-token", { refresh });
+    const response = await api.post("/refresh-token", { token });
     const content = await response.data;
 
     if (response.status !== 201) throw new Error(content.message);
