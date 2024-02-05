8894e5db0d2e163c9032eac8c08a98cf6d34f993
Pedro <pedrojuniorifpb@gmail.com>
2024-02-05 18:11:27 -0300

    Add translator in errors

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import ModalServerError from "./components/Modal/ModalCustomn/ModalServerError.js";
 import { useModal } from "./components/Modal/useModal.js";
 
+import translator from "./translator.js";
 import Router from "./router.js";
 import { api } from "./api";
 
@@ -15,7 +16,10 @@ const App = () => {
         if (error?.code === "ERR_NETWORK") {
             openModal();
         }
-        return Promise.reject(error);
+        const objError = { ...error };
+        const { message } = error.response.data;
+        objError.response.data.message = translator[message];
+        return Promise.reject(objError);
     };
 
     useEffect(() => {
---
+++ src/translator.js
@@ -0,0 +1,4 @@
+export default {
+    "Email or password not valid.": "Email ou senha invalido",
+    "Value field email already in use": "Email já em uso"
+}
